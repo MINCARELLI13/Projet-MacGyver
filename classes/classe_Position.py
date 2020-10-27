226db35d6357a95c0911a74810d4b7f99b3c48b7
E Mincarelli <sancho.poncho@laposte.net>
2020-10-27 15:52:02 +0100

    creation of a library of classes including classe_MacGyver and classe_Position

---
+++ classes/classe_Position.py
@@ -0,0 +1,9 @@
+
+class Position:
+    """ defines the coordinates of objects or characters """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+if __name__ == "__main__":
+    pass
