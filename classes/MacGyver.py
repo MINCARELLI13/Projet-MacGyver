db7f89650bdfaebdff06791ca14ac2dfad95b53a
E Mincarelli <sancho.poncho@laposte.net>
2020-10-29 14:47:00 +0100

    modification of file's name

---
+++ classes/MacGyver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from classes.Position import Position
+
+class MacGyver(Position):
+
+    def __init__(self, x, y):
+        Position.__init__(self, x, y)
+        self.bag = []
+
+    
+    def move_to (self, destination):
+        """ calculates the new coordinates after move to bottom, top, right or left of the screen """
+        coordinates = {"bottom": (self.x, self.y + 1), "top": (self.x, self.y - 1), "right": (self.x + 1, self.y), "left": (self.x - 1, self.y)}
+        return coordinates[destination]
+    
+if __name__ == "__main__":
+    mcgyver = MacGyver(8, 8)
+    print("Le personnage de MacGyver possède les coordonnées : ({}, {})".format(mcgyver.x, mcgyver.y))
+
+    
