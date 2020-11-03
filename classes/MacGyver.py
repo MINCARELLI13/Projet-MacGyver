12ad38ea2aa709d5ba32854c80655801efe1b731
E Mincarelli <sancho.poncho@laposte.net>
2020-11-03 20:32:52 +0100

    adding of comments

--- classes/MacGyver.py
+++ classes/MacGyver.py
@@ -10,12 +10,16 @@ class MacGyver(Position):
         self.bag = []
 
     def move_to(self, destination):
-        """ calculates the new coordinates after move to bottom, top, right or left of the screen """
+        """ calculates the new coordinates after move to the "destination" : bottom, top, right or left of the screen
+            In reception : receives a parameter "destination" as a direction "bottom", "top", "right" or "left"
+            In return    : return coordonates as a tuple  "(5, 8)" for example
+        """
         coordinates = {"bottom": (self.x, self.y + 1), "top": (self.x, self.y - 1), "right": (self.x + 1, self.y), "left": (self.x - 1, self.y)}
         return coordinates[destination]
     
 if __name__ == "__main__":
     mcgyver = MacGyver(8, 8)
+    print(mcgyver.move_to("bottom"))
     print("Le personnage de MacGyver possède les coordonnées : ({}, {})".format(mcgyver.x, mcgyver.y))
 
     
