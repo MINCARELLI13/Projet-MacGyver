b0417d16fa7c9dede0a3d7eec8fce575ae086418
E Mincarelli <sancho.poncho@laposte.net>
2020-11-03 20:44:04 +0100

    adding of parameter of speed of movement with the key pressed

--- classes/config.py
+++ classes/config.py
@@ -3,4 +3,6 @@
 
 OBJECTS = ["needle", "tube", "ether"]       # list of objects to find by MacGyver
 
-STEP_MOV = 40   # displacement step of MacGyver
+STEP_MOV = 40       # displacement step of MacGyver
+
+FAST = [200, 100]    # speed of movement with the key pressed
